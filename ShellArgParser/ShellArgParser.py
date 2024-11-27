e8310d8d3bb5a99dda4828cbbba27d3d170fcdec
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-27 19:48:50 +0100

    remove docstring
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- ShellArgParser/ShellArgParser.py
+++ ShellArgParser/ShellArgParser.py
@@ -8,32 +8,6 @@ class ShellArgParser:
     def __init__(
         self,
         ) -> None:
-
-        """
-        Parse arguments of script using python fire
-
-        All -args are parsed as ARGS_KEY=0 or 1
-        All --kwargs=bla are parsed as ARGS_key=value
-
-        For example:
-            ./shell_arg_parser.sh --test=something -a -b -no-c
-        Will print this text:
-            ARGS_A="1"
-            ARGS_B="1"
-            ARGS_C="0"
-            ARGS_TEST="something"
-
-        So running this:
-            eval $(./shell_arg_parser.sh --test=something -a -b -no-c)
-        Will parse the args and kwargs as environment variables, handy for use in the shell
-
-        Note:
-        -something is parsed as ARGS_SOMETHING=1
-        -no-something is parsed as ARGS_SOMETHING=0
-        -no_something is parsed as ARGS_SOMETHING=0
-        Any 'None' python value is parsed as 0
-        """
-
         # redirect the sys stdout when running fire, otherwise we sometimes get a verbose print
         sys.stdout = open(os.devnull, 'w')
         try:
